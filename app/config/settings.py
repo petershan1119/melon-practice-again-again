b2128d3a400d678eba39324850e6184e9d02628c
Sang Won Han <peter.s.han.1119@gmail.com>
2018-03-24 11:47:28 +0900

    psql로 전환

--- app/config/settings.py
+++ app/config/settings.py
@@ -103,10 +103,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ip-melon-practice-again-again',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'swhan',
+        'PASSWORD': 'rhfueo1117',
     }
 }
 
